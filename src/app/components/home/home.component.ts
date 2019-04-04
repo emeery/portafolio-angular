ad76423192bbc5199d8c7948513f65acb9b05fda
emeery <gerardoemery@live.com.mx>
2019-04-04 13:07:19 -0600

    fvicon

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,24 +4,24 @@ import { TechComponent } from '../tech/tech.component';
 import { MeComponent } from '../me/me.component';
 import { CorreoComponent } from '../correo/correo.component';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  constructor(public dlg: MatDialog, private snack: MatSnackBar) {}
+  constructor(
+    public dlg: MatDialog,
+    private snack: MatSnackBar
+  ) {}
   duracion = 5;
   openHerramientas() {
     this.dlg.open(TechComponent , {
-      width: '50%',
       panelClass: 'custom-tools'
     });
   }
   openBiografia() {
     this.dlg.open(MeComponent , {
-      width: '50%',
       panelClass: 'custom-me'
     });
   }
